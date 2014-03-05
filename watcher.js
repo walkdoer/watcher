833a154a7645b725d9d165bd288c66f126d15638
zhangmh <zhangmhao@gmail.com>
2014-03-05 20:30:08 +0800

    文件改变执行shell脚本

--- watcher.js
+++ watcher.js
@@ -2,6 +2,8 @@
 
 var fs= require('fs'),
     util = require('util'),
+    sys = require('sys'),
+    exec = require('child_process').exec,
     configFileName = 'watcher.json';
 
 util.log('------参数列表--------');
@@ -19,12 +21,27 @@ if (configStr) {
 
 util.log(watcherConfig);
 
-var files = watcherConfig.files || [];
+var files = watcherConfig.files || [],
+    processConfig = watcherConfig.process,
+    onAfterFileChange = function (file) {
+        var processType = typeof processConfig;
+        if (processType === 'function') {
+            process(file);
+        } else if (processType === 'string') {
+            exec(processConfig, function (error, stdout, stderr) {
+                // console.log(typeof stdout);
+                if (error) {
+                    throw error;
+                }
+            });
+        }
+    };
 
 files.forEach(function (file) {
     util.log('File ' + file + ' is under watch...');
     fs.watch(file, function (event) {
         console.log('File [' + file +'] ' + event);
+        onAfterFileChange(file);
     });
 });
 
