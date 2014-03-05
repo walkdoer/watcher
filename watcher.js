64730489e46aa0f9159ef8809f6f033da7b37e48
zhangmh <zhangmhao@gmail.com>
2014-03-05 19:53:59 +0800

    file watcher

--- watcher.js
+++ watcher.js
@@ -10,5 +10,24 @@ process.argv.forEach(function (val, index, array) {
     }
 });
 
-var config = fs.readFile(configFileName);
+var configStr = fs.readFileSync(configFileName, 'utf-8'),
+    watcherConfig;
+if (configStr) {
+    watcherConfig = JSON.parse(configStr);
+}
 
+console.log(watcherConfig);
+
+var files = watcherConfig.files || [];
+
+files.forEach(function (file) {
+    console.log('File ' + file + ' is under watch...');
+    fs.watch(file, function (event, fileName) {
+        console.log(arguments);
+        console.log(event);
+    });
+});
+
+process.on('exit', function(code) {
+    console.log('About to exit of watcher');
+});
